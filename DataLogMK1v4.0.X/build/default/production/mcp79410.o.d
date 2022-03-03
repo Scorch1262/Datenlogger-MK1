ef2ce871c5d487def4d8ed00b3c874d5e1af10a2
Scorch1262 <Torben.Bott@googlemail.com>
2022-03-03 19:31:18 +0100

    Add files via upload

---
+++ DataLogMK1v4.0.X/build/default/production/mcp79410.o.d
@@ -0,0 +1,21 @@
+build/default/production/mcp79410.o:  \
+mcp79410.c  \
+ mcc_generated_files/system.h  \
+mcc_generated_files/i2c1.h  \
+mcp79410.h  \
+ mcc_generated_files/mcc.h  \
+mcc_generated_files/system.h  \
+ mcc_generated_files/clock.h  \
+mcc_generated_files/pin_manager.h  \
+ mcc_generated_files/spi1_driver.h  \
+mcc_generated_files/spi1_types.h  \
+ mcc_generated_files/sd_spi/sd_spi.h  \
+mcc_generated_files/i2c1.h  \
+ mcc_generated_files/interrupt_manager.h  \
+mcc_generated_files/traps.h  \
+ mcc_generated_files/drivers/spi_master.h  \
+ mcc_generated_files/drivers/../spi1_driver.h  \
+ mcc_generated_files/fatfs/ff.h  \
+mcc_generated_files/fatfs/integer.h  \
+ mcc_generated_files/fatfs/ffconf.h  \
+command.h 
